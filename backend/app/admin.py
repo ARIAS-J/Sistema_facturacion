18879d6a58cbf8ea509a85a5140140a9e62989b4
elvstejd <elvistejeda0910@gmail.com>
2022-06-30 01:48:28 -0400

    git cache rm

--- backend/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Clientes, Articulos, Vendedores, Facturacion
-# Register your models here.
-
-admin.site.register(Clientes)
-admin.site.register(Articulos)
-admin.site.register(Vendedores)
-admin.site.register(Facturacion)

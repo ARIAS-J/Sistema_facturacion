c73d71fcb534a44c25e3b638fd19c64a04a98160
elvstejd <elvistejeda0910@gmail.com>
2022-07-06 17:36:08 -0400

    Add presentation page

---
+++ frontend/src/pages/PresentationPage.js
@@ -0,0 +1,39 @@
+import { Card, Grid, PageHeader } from '@arco-design/web-react';
+
+export default function PresentationPage() {
+  return (
+    <div>
+      <PageHeader
+        style={{ marginBottom: '1rem' }}
+        title='INTEGRANTES'
+        subTitle='2DO PARCIAL DE INTEGRACIÓN PROPIETARIA/OPEN SOURCE'
+      />
+
+      <Grid.Row gutter={16} style={{ marginBottom: 16 }} >
+        <Grid.Col span={12}>
+          <Card hoverable>
+            <Card.Meta title='Elvis Tejeda' description='A00105346' />
+          </Card>
+        </Grid.Col>
+        <Grid.Col span={12}>
+          <Card hoverable>
+            <Card.Meta title='Joel Arias' description='A00105265' />
+          </Card>
+        </Grid.Col>
+      </Grid.Row>
+      <Grid.Row gutter={16} style={{ marginBottom: 16 }}>
+        <Grid.Col span={12}>
+          <Card hoverable>
+            <Card.Meta title='Ashly Cuevas' description='A00105291' />
+          </Card>
+        </Grid.Col>
+        <Grid.Col span={12}>
+          <Card hoverable>
+            <Card.Meta title='Franlys González' description='A00105882' />
+          </Card>
+        </Grid.Col>
+      </Grid.Row>
+
+    </div>
+  )
+}

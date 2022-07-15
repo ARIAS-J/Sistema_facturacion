999333c334f6a4112944414b85e3782ea1aa5c8e
No-Hex <evilheroe1@gmail.com>
2022-07-15 17:23:17 -0400

    Added configuration JWT token

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import ClienteList, ClienteRetrieve, ArticulosList, ArticulosRetrieve, VendedoresList, VendedoresRetrieve
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     # Clientes Endpoints
     path('clientes', ClienteList),
@@ -14,4 +19,8 @@ urlpatterns = [
     # Facturacion Endpoints
     # path('facturacion', FacturacionList),
     # path('facturacion/<int:pk>/', FacturacionRetrieve)
+    
+    # Token Endpoints
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

ab49a4497f2a7bf8e9d8b0aa6a30a185216c7668
elvstejd <elvistejeda0910@gmail.com>
2022-07-25 19:47:16 -0400

    Improve *fake* auth logic

--- frontend/src/contexts/Auth.js
+++ frontend/src/contexts/Auth.js
@@ -27,15 +27,23 @@ function AuthProvider({ children }) {
   useEffect(() => {
     const refresh = localStorage.getItem('refresh');
 
-    if (refresh) {
-      axios.post('http://localhost:8000/api/v1/token/refresh/', { refresh: refresh }).then(({ data }) => {
-        console.log(data)
-        setCurrentUser({ access: data.access, refresh: refresh });
-        setLoading(false);
-      });
-    } else {
+    if (!refresh) {
       setLoading(false);
+      console.log('not refresh');
+      return;
     }
+
+
+    axios.post('http://localhost:8000/api/v1/token/refresh/', { refresh: refresh }).then(({ data }) => {
+      console.log(data)
+      setCurrentUser({ access: data.access, refresh: refresh });
+      console.log('user found');
+      setLoading(false);
+    }).catch((e) => {
+      console.log(e);
+      setLoading(false);
+    });
+
   }, []);
 
   const value = {
@@ -47,7 +55,12 @@ function AuthProvider({ children }) {
 
   return (
     <AuthContext.Provider value={value}>
-      {!loading && children}
+      {loading ? (<div style={{
+        minHeight: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>Cargando...</div>) : (children)}
     </AuthContext.Provider>
   )
 }
